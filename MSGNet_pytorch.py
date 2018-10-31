e345a389091d7c19be1d06f7c77b33c071ebbf13
Gleb Bobrovskikh <bobrovskihg@gmail.com>
2018-10-31 09:44:34 +0300

    added train functions and dataset class

--- MSGNet_pytorch.py
+++ MSGNet_pytorch.py
@@ -2,10 +2,13 @@ from torch import nn
 from torch.nn import functional as F
 import torch
 from torchvision import models
+from torch.utils.data import Dataset, DataLoader
 import torchvision
 import numpy as np
 from scipy.misc import imresize
 from scipy.ndimage import convolve
+import cv2 
+import h5py
 
 # useful functions
 def normalize_cleanIm(Z, min_Z, max_Z):
@@ -24,10 +27,55 @@ def rgb2ycbcr(im_rgb):
     im_ycbcr[:,:,1:] = (im_ycbcr[:,:,1:]*(240-16)+16)/255.0 #to [16/255, 240/255]
     return im_ycbcr
 
+# loss function
+class RMSELoss(torch.nn.Module):
+    def __init__(self):
+    	super(RMSELoss,self).__init__()
+
+    def forward(self, x, y):
+        criterion = nn.MSELoss()
+        loss = torch.sqrt(criterion(x, y))
+        return loss
+
+# dataset class
+class MSGNet_dataset(Dataset):
+    """Default MSGNet dataset."""
+
+    def __init__(self, root_dir, transform=None):
+        """
+        Args:
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.root_dir = root_dir
+        self.transform = transform
+        f = h5py.File(root_dir + 'Df.mat', 'r')
+        dataset_shape = f['Df'].shape
+
+    def __len__(self):
+        return dataset_shape[1]
+
+    def __getitem__(self, idx):
+        # get rgb image
+        imgs = h5py.File(root_dir + 'RGB.mat', 'r')
+        img = imgs[imgs['RGB'][0][idx]][:]
+        
+        # get deep map for image
+        dfs = h5py.File(root_dir + 'Df.mat', 'r')
+        df = dfs[dfs['Df'][0][idx]][:]
+        
+        sample = [img, dh]
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
+
+
 # MSGNet 
 class ConvPReLu(nn.Module):
     def __init__(self, in_channels, out_channels):
-        super().__init__()
+        super(ConvPReLu, self).__init__()
         self.conv = nn.Conv2d(in_channels, out_channels, 5, stride=1, padding=2)
         self.activation = nn.PReLU()
 
@@ -38,7 +86,7 @@ class ConvPReLu(nn.Module):
     
 class DeconvPReLu(nn.Module):
     def __init__(self, in_channels, out_channels):
-        super().__init__()
+        super(ConvPReLu, self).__init__()
         self.conv = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=3, stride=2, padding=1)
         self.activation = nn.PReLU(inplace=True)
 
@@ -50,7 +98,7 @@ class DeconvPReLu(nn.Module):
 
 class MSGNet(nn.Module):
     def __init__(self, upsample=2):
-        super().__init__()
+        super(MSGNet, self).__init__()
         # initialize indexes for layers
         self.upsample = upsample
         m = int(np.log2(upsample))
@@ -69,7 +117,7 @@ class MSGNet(nn.Module):
         self.feature_extraction_Y.append(nn.PReLU(nn.Conv2d(in_, out, 7, stride=1, padding=3)))
         in_, out = 49, 32
         self.feature_extraction_Y.append(ConvPReLu(in_, out))
-        for i in range(2, 2m):
+        for i in range(2, 2*m):
             if i in j:
                 self.feature_extraction_Y.append(ConvPReLu(in_, out))
             if i in j_:
@@ -90,7 +138,7 @@ class MSGNet(nn.Module):
         self.upsampling_X.append(ConvPReLu(in_, out, 5, stride=1, padding=2)) # reconstruction        
         
     def forward(self, gt, rgb):
-        # early spectral decomposition
+    	# early spectral decomposition
         h = np.ones(3,3)/9
         
         im_Dl = imresize(float(gt),  self.upsample, 'bicubic')
@@ -100,7 +148,7 @@ class MSGNet(nn.Module):
         in_D = im_Dl - im_Dl_LF
         
         # Y-channel 
-        im_I = rgb2ycbcr(ims_RGB{k_sub})
+        im_I = rgb2ycbcr(rgb)
         im_Y = float(im_I[:,:,1])
             
         im_Y = normalize_cleanIm(im_Y);
@@ -136,4 +184,47 @@ class MSGNet(nn.Module):
         im_Dh = im_Dh*(max_D - min_D) + min_D
         
         return output
-            
+
+def train(model, train_loader, optimizer, loss, device, epoch):
+    model.train()
+    train_loss = 0
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        train_loss = loss(output, target) 
+        train_loss.backward()
+        optimizer.step()
+        if batch_idx % 100 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                    epoch, batch_idx * len(data), len(train_loader.dataset),
+                    100. * batch_idx / len(train_loader), train_loss.item()))
+
+def test(model, test_loader, loss, device, epoch):
+    model.eval()
+    test_loss = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += loss(output, target).item()
+    test_loss /= len(test_loader.dataset)
+    print('\nTest set: Average loss: {:.4f}\n'.format(test_loss))
+
+if __name__ == '__main__':
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    #Assume that we are on a CUDA machine, then this should print a CUDA device:
+    print(device)
+    train = MSGNet_dataset(root_dir='../datasets/MSGNet_data/')
+    train_loader = DataLoader(train, batch_size=4, shuffle=True)
+    #test = 
+    #test_loader = DataLoader(test, batch_size=4, shiffle=True)
+    model = MSGNet(upsample=2).to(device)
+    optimizer = torch.optim.SGD(model.parameters(), 10e-3,
+                                momentum=0.9)
+    loss = RMSELoss()
+    for epoch in range(epochs):
+        train(model, train_loader, optimizer, loss, device, epoch)
+        #test(model, test_loader, loss, device, epoch)
+
+
